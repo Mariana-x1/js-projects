ef4e5fca89f266e56b7b3fa16ff8a5c9435f3c8a
Mari Astamadze <marihamburg321@gmail.com>
2024-02-16 00:12:33 +0100

    Coding-Task: Use operators & data types

---
+++ coding-task-2-use-operators-and-data-types/script.js
@@ -0,0 +1,38 @@
+// 01) Write a String that logs out your first name
+/*Use the concatenation operator to concatenate each 
+letter of your firstname into a string one letter by one.*/
+console.log("M" + "A" + "R" + "I");
+
+/*02) Spot the problems & solve them
+ Copy the following code snippets 
+ to the script.js file and try to spot and fix the errors. */
+
+/* Error! Make it work
+console.log("Hallo";*/
+console.log("Hello");
+
+/* Error! Make it work
+console.log("That doesn"t work"); */
+console.log("That doesn't work");
+
+/* should return true. Change only the operator to fix it.
+console.log(50 + 2 > 52); */
+console.log(50 + 2 === 52);
+
+/* should be false. Change only the operator to fix it.
+console.log("333" == 333);*/
+console.log("333" === 333);
+
+/* Wrong result: Expected 555 not 855. Change only the operator to fix it.
+console.log(600 + 510 / 2); */
+console.log((600 + 510) / 2);
+
+/*// Wrong result: Expected a Number 4 received 22
+console.log("2" + 2); */
+console.log(2 + 2);
+
+/* 03) Compare two conditions
+ Create a console.log that returns true when both division results are equal to 5
+console.log(25 / 5); // Result: 5
+console.log(50 / 10); // Result: 5 */
+console.log(25 / 5 === 50 / 10);
